3b7311e3bdfe84929c9e21b486f11e98af22f086
AG-gitt <143864585+AG-gitt@users.noreply.github.com>
2023-12-07 13:46:11 +0330

    Add files via upload

--- proxy.cpp
+++ proxy.cpp
@@ -8,11 +8,12 @@ void halat(int a);
 void halat(int a)
 {
     int t = a;
+    int octet[3];
     int list[12];
     int n=4;
     if(a<1000)
     {
-        cout<<"IP dade shode emkanpazir nist";
+        cout<<"IP dade shode emkanpazir nist"<<endl;
     }
     while(a>999)
     {
@@ -23,7 +24,7 @@ void halat(int a)
         }
     }
     a=t;
-    for(int i = 0 ; i<n ; i++)
+    for(int i = n-1 ; i>=0 ; i--)
     {
         list[i] = a % 10 ;
         a=a/10;
@@ -32,11 +33,29 @@ void halat(int a)
     {
         cout<<list[i]<<endl;
     }
-}
+    if(n>4)
+    {
+        for(int i = 0 ; i<4 ; i++)
+        {
+            for(int j = 0 ; j<n ; j++)
+            {
+                octet[i]=list[n]+octet[i];
+                if(octet[j]<256)
+                {
+                    
+                }
+            }
+        }
+        if(n=4)
+        {
+            cout<<list[0]<<"."<<list[1]<<"."<<list[2]<<"."<<list[3]<<endl;
+        }
+    }
+}   
 int main(void)
 {
     int a;
-    cout<<"IP ro bede: ";
+    cout<<"IP ro bede: "<<endl;
     cin>>a;
     halat(a);
 }
